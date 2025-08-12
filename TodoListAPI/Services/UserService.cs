49c69a4052d45d31ab9c19b57903c0000dad4a67
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-08-11 23:40:32 -0400

    Add UserService

---
+++ TodoListAPI/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using TodoListAPI.Models;
+
+namespace TodoListAPI.Services;
+
+public interface IUserService
+{
+    Task<UserModel?> GetUserByEmail(string email);
+    Task<UserModel?> GetUserById(int userId);
+}
+
+public class UserService : IUserService
+{
+    private readonly string _connectionString;
+    
+    public UserService(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("TodoList")
+                            ?? throw new InvalidOperationException("TodoList connection string not found in appsettings.json");
+    }
+    
+    public async Task<UserModel?> GetUserByEmail(string email)
+    {
+        await using var conn = new SqlConnection(_connectionString);
+        await conn.OpenAsync();
+
+        return await conn.QuerySingleOrDefaultAsync<UserModel>(
+            "spGetUserByEmail",
+            new { Email = email },
+            commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<UserModel?> GetUserById(int userId)
+    {
+        var sql = "SELECT * FROM [Users] WHERE UserId = @UserId";
+        
+        await using var conn = new SqlConnection(_connectionString);
+        await conn.OpenAsync();
+
+        return await conn.QuerySingleOrDefaultAsync<UserModel>(sql,new { UserId = userId });
+    }
+}
