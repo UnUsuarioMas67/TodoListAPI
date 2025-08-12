75438a5efb9434f299871a6c5d270917c22d34c9
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-08-11 20:46:45 -0400

    Initial commit

---
+++ TodoListAPI/Models/UserLogin.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoListAPI.Models;
+
+public class UserLogin
+{
+    [StringLength(254)] public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ TodoListAPI/Models/UserRegister.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoListAPI.Models;
+
+public class UserRegister
+{
+    public string Username { get; set; }
+    [StringLength(254)] public string Email { get; set; }
+    public string Password { get; set; }
+}
